6c56861dc823a8dada13272a348dc6f87f20cd01
Jo Wagner <jowagner@limago.de>
2022-03-14 10:07:25 +0100

    Dynamic Proxy

--- tag1_01calculator/src/de/gothaer/math/CalculatorFactory.java
+++ tag1_01calculator/src/de/gothaer/math/CalculatorFactory.java
@@ -1,12 +1,15 @@
 package de.gothaer.math;
 
+import de.gothaer.common.LoggerProxy;
+
 public class CalculatorFactory {
 
     private static boolean logger = true;
     private static boolean secure = false;
     public static Calculator create() {
         Calculator calculator = new CalculatorImpl();
-        if(logger) calculator = new CalculatorLogger(calculator);
+        //if(logger) calculator = new CalculatorLogger(calculator);
+        if(logger) calculator = (Calculator) LoggerProxy.newInstance(calculator);
         if (secure) calculator = new CalculatorSecure(calculator);
         return calculator;
     }
