c9b41ea133beca2b5e5a00ec634a3cc0dfdb7634
Jo Wagner <jowagner@limago.de>
2022-03-14 09:41:44 +0100

    Initial commit

--- tag1_01calculator/src/de/gothaer/Main.java
+++ tag1_01calculator/src/de/gothaer/Main.java
@@ -3,11 +3,18 @@ package de.gothaer;
 import de.gothaer.client.CalcClient;
 import de.gothaer.math.*;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        Instant start = Instant.now();
         CalcClient client = new CalcClient(CalculatorFactory.create());
         client.run();
+        Instant ende = Instant.now();
+
+        Duration duration =Duration.between(start, ende);
+        System.out.println("Duration = " + duration.toMillis());
     }
 }
--- tag1_01calculator/src/de/gothaer/math/CalculatorImpl.java
+++ tag1_01calculator/src/de/gothaer/math/CalculatorImpl.java
@@ -6,7 +6,12 @@ public class CalculatorImpl implements Calculator {
 
     @Override
     public double add(double a, double b) {
-       return a + b;
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return a + b;
     }
     @Override
     public double sub(double a, double b) {
